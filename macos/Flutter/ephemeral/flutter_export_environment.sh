b4e4ba5a34fa96d9d392ff5596729291302d27b3
Maurizio Pinotti <maurizio.pinotti@cnhind.com>
2024-06-12 11:35:49 +0200

    Upgrade pub deps
    - loosen constraints of intl package
    - update to latest flutter_lints

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\gaspa\tools\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\gaspa\Documents\Dev\OpenSource\Pub\date_range_picker"
+export "FLUTTER_ROOT=/opt/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/mauriziopinotti/Projects/easyhour/date_range_picker"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.0.1"
-export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "FLUTTER_BUILD_NAME=0.0.14"
+export "FLUTTER_BUILD_NUMBER=0.0.14"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
